c40fc2ef84bb8eb0bd5fc6515b4bc1f3184bb7c0
Andrew Kalenda <andrew.kalenda@gmail.com>
2015-03-30 17:06:11 -0700

    Integrated Error-Prone java compiler

--- Java/src/main/java/com/akalenda/MoogleKiwi/HelloWorld.java
+++ Java/src/main/java/com/akalenda/MoogleKiwi/HelloWorld.java
@@ -7,4 +7,13 @@ public class HelloWorld {
     public static String get() {
         return "Hello, world!";
     }
+
+    /*
+     * Some temporary code to check that Error-Prone is working
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            new IllegalArgumentException("Missing required argument");
+        }
+    }
+    */
 }
