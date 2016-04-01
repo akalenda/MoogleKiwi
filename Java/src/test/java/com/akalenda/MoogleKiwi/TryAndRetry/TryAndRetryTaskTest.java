d2f571452afed6c5dd4fdb336d5f0c5d7813b450
akalenda <blah@blah.com>
2016-04-01 13:28:42 -0700

    Added assertions to make that all threads are actually executed. If the assertions within are not given the chance to fail, then tests would always pass regardless of whether they actually succeed.

--- Java/src/test/java/com/akalenda/MoogleKiwi/TryAndRetry/TryAndRetryTaskTest.java
+++ Java/src/test/java/com/akalenda/MoogleKiwi/TryAndRetry/TryAndRetryTaskTest.java
@@ -20,6 +20,7 @@ public class TryAndRetryTaskTest extends TestCase {
      * @throws Exception
      */
     public void testWithAsyncExceptionHandler() throws Exception {
+        AtomicBoolean executed = new AtomicBoolean(false);
         Instant start = Instant.now();
         AtomicBoolean exceptionHasNotBeenThrown = new AtomicBoolean(true);
         Semaphore waitForExceptionHandlerToFinish = new Semaphore(0);
@@ -36,6 +37,7 @@ public class TryAndRetryTaskTest extends TestCase {
                     }
                 })
                 .executeUntilDoneThenGet(() -> {
+                    executed.set(true);
                     if (exceptionHasNotBeenThrown.get()) {
                         exceptionHasNotBeenThrown.set(false);
                         throw new Exception("Force the exception handler to trigger");
@@ -46,6 +48,7 @@ public class TryAndRetryTaskTest extends TestCase {
         assertTrue(timeToFinish < 5 * 1000);
         boolean exceptionHandlerFinished = waitForExceptionHandlerToFinish.tryAcquire(10 * 10000, TimeUnit.MILLISECONDS);
         assertTrue(exceptionHandlerFinished);
+        assertTrue(executed.get());
     }
 
     /**
@@ -54,6 +57,7 @@ public class TryAndRetryTaskTest extends TestCase {
      * @throws Exception
      */
     public void testWithBlockingExceptionHandler() throws Exception {
+        AtomicBoolean executed = new AtomicBoolean(false);
         Semaphore waitForExceptionToBeHandled = new Semaphore(0);
         Boolean handlerWorked = TryAndRetry
                 .withAttemptsUpTo(2)
@@ -62,11 +66,13 @@ public class TryAndRetryTaskTest extends TestCase {
                     return true;
                 })
                 .executeUntilDoneThenGet(() -> {
+                    executed.set(true);
                     if (waitForExceptionToBeHandled.tryAcquire())
                         return true;
                     throw new Exception("Trigger the handler");
                 });
         assertEquals(Boolean.TRUE, handlerWorked);
+        assertTrue(executed.get());
     }
 
     /**
@@ -75,6 +81,7 @@ public class TryAndRetryTaskTest extends TestCase {
      * @throws Exception
      */
     public void testWithBlockingExceptionHandler_actuallyIsBlocking() throws Exception {
+        AtomicBoolean executed = new AtomicBoolean(false);
         AtomicBoolean exceptionHasNotBeenThrown = new AtomicBoolean(true);
         AtomicBoolean sleepWasUninterrupted = new AtomicBoolean();
         AtomicBoolean exceptionHasUnblocked = new AtomicBoolean(false);
@@ -93,48 +100,58 @@ public class TryAndRetryTaskTest extends TestCase {
                     }
                 })
                 .executeUntilDoneThenGet(() -> {
+                    executed.set(true);
                     if (exceptionHasNotBeenThrown.get())
                         throw new Exception("...so as to trigger the exception handler");
                     return exceptionHasUnblocked.get();
                 });
         assertEquals(executionResult, true);
+        assertTrue(executed.get());
     }
 
     public void testWithBlockingExceptionHandler_abort() throws Exception {
+        AtomicBoolean executed = new AtomicBoolean(false);
         boolean handlerAbortWorked;
         try {
             handlerAbortWorked = TryAndRetry
                     .withAttemptsUpTo(1)
                     .withBlockingExceptionHandler(exception -> false)
                     .executeUntilDoneThenGet(() -> {
+                        executed.set(true);
                         throw new Exception("Force failure and trigger exception");
                     });
         } catch (TryAndRetryFailuresException e) {
             handlerAbortWorked = true;
         }
         assertTrue(handlerAbortWorked);
+        assertTrue(executed.get());
     }
 
     public void testExecuteAsync() throws Exception {
+        AtomicBoolean executed = new AtomicBoolean(false);
         Semaphore lambdaWaitsForMainThreadToRelease = new Semaphore(0);
         CompletableFuture<Boolean> asynchWorked = TryAndRetry
                 .withAttemptsUpTo(20)
                 .withWaitPeriod(1, TimeUnit.SECONDS)
                 .executeAsync(() -> {
+                    executed.set(true);
                     if (lambdaWaitsForMainThreadToRelease.tryAcquire())
                         return true;
                     throw new Exception("...so as to force retries");
                 });
         lambdaWaitsForMainThreadToRelease.release();
         assertTrue(asynchWorked.get(25, TimeUnit.SECONDS));
+        assertTrue(executed.get());
     }
 
     public void testExecuteUntilDoneThenGet() throws Exception {
+        AtomicBoolean executed = new AtomicBoolean(false);
         Instant start = Instant.now();
         long timeToSleepMillis = 2 * 1000;
         Instant end = TryAndRetry
                 .withAttemptsUpTo(1)
                 .executeUntilDoneThenGet(() -> {
+                    executed.set(true);
                     try {
                         Thread.sleep(timeToSleepMillis);
                         return Instant.now();
@@ -144,14 +161,18 @@ public class TryAndRetryTaskTest extends TestCase {
                 });
         long timeToFinish = Duration.between(start, end).toMillis();
         assertTrue(timeToFinish >= timeToSleepMillis);
+        assertTrue(executed.get());
     }
 
     public void testContinueUntilDoneThenGet() throws Exception {
+        AtomicBoolean executed = new AtomicBoolean(false);
+        AtomicBoolean executed2 = new AtomicBoolean(false);
         int attemptsFirstWave = 20;
         int attemptsTotal = 55;
         AtomicInteger totalAttemptsMade = new AtomicInteger(0);
         TryAndRetryTask testTask = TryAndRetry.withAttemptsUpTo(attemptsTotal);
         testTask.executeUntilDoneThenGet(() -> {
+            executed.set(true);
             totalAttemptsMade.incrementAndGet();
             if (totalAttemptsMade.get() < attemptsFirstWave)
                 throw new Exception("...so as to retry 20 times");
@@ -160,12 +181,15 @@ public class TryAndRetryTaskTest extends TestCase {
         assertEquals(attemptsFirstWave, totalAttemptsMade.get());
         try {
             testTask.continueUntilDoneThenGet(() -> {
+                executed2.set(true);
                 totalAttemptsMade.incrementAndGet();
                 throw new Exception("...so as to continue retrying until all attempts are used");
             });
         } catch (TryAndRetryFailuresException ignored) {
         }
         assertEquals(attemptsTotal, totalAttemptsMade.get());
+        assertTrue(executed.get());
+        assertTrue(executed2.get());
     }
 
     /**
@@ -174,12 +198,14 @@ public class TryAndRetryTaskTest extends TestCase {
      * @throws Exception
      */
     public void testWithNoWaitPeriod() throws Exception {
+        AtomicBoolean executed = new AtomicBoolean(false);
         AtomicBoolean exceptionHasNotBeenThrown = new AtomicBoolean(true);
         AtomicLong millisecondsOfStart = new AtomicLong(0);
         long lengthOfWaitPeriod = TryAndRetry
                 .withAttemptsUpTo(2)
                 .withNoWaitPeriod()
                 .executeUntilDoneThenGet(() -> {
+                    executed.set(true);
                     long millisecondsOfEnd = System.currentTimeMillis();
                     if (exceptionHasNotBeenThrown.get()) {
                         exceptionHasNotBeenThrown.set(false);
@@ -189,6 +215,7 @@ public class TryAndRetryTaskTest extends TestCase {
                     return millisecondsOfEnd - millisecondsOfStart.get();
                 });
         assertEquals(0, lengthOfWaitPeriod);
+        assertTrue(executed.get());
     }
 
     /**
@@ -197,6 +224,7 @@ public class TryAndRetryTaskTest extends TestCase {
      * @throws Exception
      */
     public void testLogarithmicallyIncreasing() throws Exception {
+        AtomicBoolean executed = new AtomicBoolean(false);
         Box<Instant> startOfWaitPeriod = new Box<>();
         Box<Duration> durationOfPreviousWaitPeriod = new Box<>();
         try {
@@ -205,6 +233,7 @@ public class TryAndRetryTaskTest extends TestCase {
                     .withWaitPeriod(100, TimeUnit.MILLISECONDS)
                     .logarithmicallyIncreasing()
                     .executeUntilDoneThenGet(() -> {
+                        executed.set(true);
                         if (startOfWaitPeriod.hasContents()) {
                             Duration durationOfCurrentWaitPeriod = Duration.between(startOfWaitPeriod.unbox(), Instant.now());
                             double expectedCurrentWaitPeriod = ((double) durationOfPreviousWaitPeriod.unbox().toMillis()) * 1.5;
@@ -217,6 +246,7 @@ public class TryAndRetryTaskTest extends TestCase {
                     });
         } catch (TryAndRetryFailuresException ignored) {
         }
+        assertTrue(executed.get());
     }
 
     /**
@@ -225,6 +255,7 @@ public class TryAndRetryTaskTest extends TestCase {
      * @throws Exception
      */
     public void testLinearlyIncreasingBy() throws Exception {
+        AtomicBoolean executed = new AtomicBoolean(false);
         Box<Instant> startOfWaitPeriod = new Box<>();
         Box<Duration> durationOfPreviousWaitPeriod = new Box<>();
         ImmutableList.Builder<Duration> differencesInWaitPeriods = new ImmutableList.Builder<>();
@@ -234,6 +265,7 @@ public class TryAndRetryTaskTest extends TestCase {
                     .withNoWaitPeriod()
                     .linearlyIncreasingBy(500, TimeUnit.MILLISECONDS)
                     .executeUntilDoneThenGet(() -> {
+                        executed.set(true);
                         if (startOfWaitPeriod.hasContents()) {
                             Duration durationOfCurrentWaitPeriod = Duration.between(startOfWaitPeriod.unbox(), Instant.now());
                             Duration differenceBetweenWaitPeriods = durationOfCurrentWaitPeriod.minus(durationOfPreviousWaitPeriod.unbox());
@@ -250,6 +282,7 @@ public class TryAndRetryTaskTest extends TestCase {
             System.out.println(millis);
             assertTrue(499 <= millis && millis <= 501);
         });
+        assertTrue(executed.get());
     }
 
     /**
@@ -258,6 +291,7 @@ public class TryAndRetryTaskTest extends TestCase {
      * @throws Exception
      */
     public void testUpTo() throws Exception {
+        AtomicBoolean executed = new AtomicBoolean(false);
         Box<Instant> startOfWaitPeriod = new Box<>();
         Instant startTimer = Instant.now();
         try {
@@ -267,6 +301,7 @@ public class TryAndRetryTaskTest extends TestCase {
                     .linearlyIncreasingBy(10, TimeUnit.SECONDS)
                     .upTo(2, TimeUnit.SECONDS)
                     .executeUntilDoneThenGet(() -> {
+                        executed.set(true);
                         if (startOfWaitPeriod.hasContents()) {
                             long secondsBetweenAttempts = Duration.between(startOfWaitPeriod.unbox(), Instant.now()).getSeconds();
                             assertTrue("Seconds: " + secondsBetweenAttempts, secondsBetweenAttempts <= 2);
@@ -276,8 +311,24 @@ public class TryAndRetryTaskTest extends TestCase {
                     });
         } catch (TryAndRetryFailuresException ignored) {
         }
+        assertTrue(executed.get());
     }
 
+    public void testCollectedExceptions() {
+        AtomicBoolean executed = new AtomicBoolean(false);
+        AtomicInteger counter = new AtomicInteger(0);
+        try {
+            TryAndRetry
+                    .withAttemptsUpTo(20)
+                    .executeUntilDoneThenGet(() -> {
+                        executed.set(true);
+                        throw new Exception("Number " + counter.incrementAndGet());
+                    });
+        } catch (TryAndRetryFailuresException e) {
+            assertTrue(e.getSuppressed().length == 20);
+        }
+        assertTrue(executed.get());
+    }
 
     /**
      * Simple container class, because Java lambdas still don't do closure properly... >:[
